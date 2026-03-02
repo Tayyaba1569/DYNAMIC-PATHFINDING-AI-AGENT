1e78813d16935fea54c2c48f1d17d6668c5a5ad8
Tayyaba1569 <tayyabafarooq493@gmail.com>
2026-03-02 20:52:10 +0500

    Update algorithms.py

---
+++ algorithms.py
@@ -0,0 +1,65 @@
+# algorithms.py
+
+import heapq
+import math
+
+
+def manhattan(a, b):
+    return abs(a.row - b.row) + abs(a.col - b.col)
+
+
+def euclidean(a, b):
+    return math.sqrt((a.row - b.row)**2 + (a.col - b.col)**2)
+
+
+def reconstruct_path(goal):
+    path = []
+    current = goal
+    while current.parent:
+        path.append(current)
+        current = current.parent
+    path.append(current)
+    return path
+
+
+def search(grid, start, goal, heuristic, use_g_cost=True):
+    open_set = []
+    count = 0
+
+    start.g = 0
+    start.f = heuristic(start, goal)
+
+    heapq.heappush(open_set, (start.f, count, start))
+
+    visited_nodes = []
+
+    while open_set:
+        current = heapq.heappop(open_set)[2]
+
+        if current.is_wall:
+            continue
+
+        current.is_visited = True
+        visited_nodes.append(current)
+
+        if current == goal:
+            return reconstruct_path(goal), visited_nodes
+
+        for neighbor in grid.get_neighbors(current):
+
+            temp_g = current.g + 1
+
+            if use_g_cost:
+                new_f = temp_g + heuristic(neighbor, goal)
+            else:
+                new_f = heuristic(neighbor, goal)
+
+            if new_f < neighbor.f:
+                neighbor.parent = current
+                neighbor.g = temp_g
+                neighbor.f = new_f
+                neighbor.is_frontier = True
+                count += 1
+                heapq.heappush(open_set, (neighbor.f, count, neighbor))
+
+    return None, visited_nodes
